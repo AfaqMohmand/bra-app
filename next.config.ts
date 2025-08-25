4ebad00e66c8bcf2b9964f0141f3f005b5d76274
Afaq Ahmad <afaq3167710@gmail.com>
2025-08-25 22:37:27 +0500

    fucking changes reverterd

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  distDir: "out",
   images: {
     unoptimized: true,
     domains: [
