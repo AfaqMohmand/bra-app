bd51f41071c1fca10860a28e06782b933c66f42a
Afaq Ahmad <afaq3167710@gmail.com>
2025-08-25 20:30:04 +0500

    server related changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: "out",
   images: {
     unoptimized: true,
     domains: [
